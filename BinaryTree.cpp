660ddb25301661639b2d13a33d0b32afbf116469
Aditya <adityagaba1322@gmail.com>
2024-01-03 22:54:28 +0530

    add

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -236,4 +236,4 @@ int main()
 // 1 2 3 4 5 6 7 -1 -1 -1 -1 8 9 -1 -1 -1 -1 -1 -1
 // 1 2 3 4 5 -1 -1  6 -1 -1 7 8 -1 -1 9 -1 -1 -1 -1
 
-// for using recursion we use this input : 1 2 4 -1 -1 5 -1 -1 3 6 -1 -1 7 -1 -1
+// for using recursion we use this input : 1 2 4 -1 -1 5 6 -1 -1 7 -1 -1 3 8 -1 -1 9 -1 -1
