91c1ef425a40d704bcbaf3f74a3f0737adbdd941
Aditya <adityagaba1322@gmail.com>
2024-01-08 19:17:46 +0530

    add

--- leetcode.cpp
+++ leetcode.cpp
@@ -32992,6 +32992,6 @@ private:
 
 
 
-// #pragma GCC optimize("Ofast","inline","fast-math","unroll-loops","no-stack-protector")
-// #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
-// static const auto fast = []() {ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0); return 0; } ();
+#pragma GCC optimize("Ofast","inline","fast-math","unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
+static const auto fast = []() {ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0); return 0; } ();
