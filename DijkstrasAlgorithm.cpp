f3e7bce8342330db7fc6fb1f171b2b4d93b69020
Aditya <adityagaba1322@gmail.com>
2024-01-31 16:25:37 +0530

    q

--- DijkstrasAlgorithm.cpp
+++ DijkstrasAlgorithm.cpp
@@ -1,78 +1,86 @@
-#include<iostream>   // Time Complexity : O(n^2) , Space Complexity : O(n^2)
-#include<climits>  // Change Time Complexity to O(ElogV) using Min Heap and then to O(E+VlogV) using Fibonacci Heap
+#include <iostream> // Time Complexity : O(n^2) , Space Complexity : O(n^2)
+#include <climits>  // Change Time Complexity to O(ElogV) using Min Heap and then to O(E+VlogV) using Fibonacci Heap
 using namespace std;
 
-int findMinVertex(int *distance , bool *visited , int n){
+int findMinVertex(int *distance, bool *visited, int n)
+{
     int minVertex = -1;
-    for(int i=0;i<n;i++){
-        if(!visited[i] && (minVertex == -1 || distance[i] < distance[minVertex])){
+    for (int i = 0; i < n; i++)
+    {
+        if (!visited[i] && (minVertex == -1 || distance[i] < distance[minVertex]))
+        {
             minVertex = i;
         }
     }
     return minVertex;
 }
 
-void dijkstra(int **edges , int n){
+void dijkstra(int **edges, int n)
+{
     int *distance = new int[n];
     bool *visited = new bool[n];
-    for(int i=0;i<n;i++){
+    for (int i = 0; i < n; i++)
+    {
         distance[i] = INT_MAX;
         visited[i] = false;
     }
     distance[0] = 0;
-    for(int i=0;i<n-1;i++){
-        int minVertex = findMinVertex(distance,visited,n);
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minVertex = findMinVertex(distance, visited, n);
         visited[minVertex] = true;
-        for(int j=0;j<n;j++){
-            if(edges[minVertex][j] != 0 && !visited[j]){
+        for (int j = 0; j < n; j++)
+        {
+            if (edges[minVertex][j] != 0 && !visited[j])
+            {
                 int dist = distance[minVertex] + edges[minVertex][j];
-                if(dist < distance[j]){
+                if (dist < distance[j])
+                {
                     distance[j] = dist;
                 }
             }
         }
-
     }
-    for(int i=0;i<n;i++){
+    for (int i = 0; i < n; i++)
+    {
         cout << i << " " << distance[i] << endl;
     }
 
-
-  
-    delete [] distance;
-    delete [] visited;
-
+    delete[] distance;
+    delete[] visited;
 }
 
-int main(){
-      int n;
+int main()
+{
+    int n;
     int e;
-    cin>>n>>e;
-    int **edges = new int*[n];
-    for(int i=0;i<n;i++){
+    cin >> n >> e;
+    int **edges = new int *[n];
+    for (int i = 0; i < n; i++)
+    {
         edges[i] = new int[n];
-        for(int j=0;j<n;j++){
+        for (int j = 0; j < n; j++)
+        {
             edges[i][j] = 0;
         }
     }
-    for(int i=0;i<e;i++){
-        int f,s,weight;
-        cin>>f>>s>>weight;
+    for (int i = 0; i < e; i++)
+    {
+        int f, s, weight;
+        cin >> f >> s >> weight;
         edges[f][s] = weight;
         edges[s][f] = weight;
     }
     cout << endl;
-    dijkstra(edges,n);
-    for(int i=0;i<n;i++){
-        delete [] edges[i];
+    dijkstra(edges, n);
+    for (int i = 0; i < n; i++)
+    {
+        delete[] edges[i];
     }
-    delete [] edges;
-    
+    delete[] edges;
+
     return 0;
-    
-
 }
-
 
 // INPUT:
 // 5 7
