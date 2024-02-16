abf7a9ab19f485d93f24ec8806a1bf141d60d84f
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2024-02-16 22:48:31 +0530

    a

--- codeforces/CONTESTS/Round 926 (Div. 2)/A.SashaandtheBeautifulArray.cpp
+++ codeforces/CONTESTS/Round 926 (Div. 2)/A.SashaandtheBeautifulArray.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 
 int main(){
-    cout << "Hello World";
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        int a[n];
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+        }
+        sort(a, a + n);
+        int diff = 0;
+        for(int i = 0; i < n-1; i++){
+            diff += a[i+1] - a[i];
+        }
+        cout << diff << endl;
+    }
+
     return 0;
 }
