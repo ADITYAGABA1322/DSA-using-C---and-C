8d11155be360647d9320ff577e8fca96ab2e4dae
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2024-07-21 00:12:15 +0530

    add

--- leetcode.cpp
+++ leetcode.cpp
@@ -31759,6 +31759,45 @@ public:
 
 
 
+/*
+	Name :- Tafheem Ahemad
+*/
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll long long int
+#define ld long double
+#define F first
+#define S second
+const int MOD=1e9+7;
+#define vl vector<ll>
+#define vi vector<int>
+#define vs vector<string>
+#define vll vector<vl>
+#define vii vector<vi>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define ump unordered_map
+#define all(v) v.begin(),v.end()
+#define take_matrix(a,n,m) for(int i=0; i<n; i++){for(int j=0; j<m; j++){cin>>a[i][j];}}
+#define print_matrix(a,n,m) for(int i=0; i<n; i++){for(int j=0; j<m; j++){cout<<a[i][j]<<' ';}cout<<endl;}
+#define input(a) for(auto &x:a) cin>>x;
+#define printv(a) for(auto &i:a) cout<<i<<' '; cout<<endl;
+#define yes(x) if(x) cout<<"YES"<<endl; else cout<<"NO"<<endl;
+#define cl(n) for(int i=0;i<=n+5;i++) g[i].clear()
+#define mem(v,z) memset(v,z,sizeof(v))
+#define bpc(x) __builtin_popcountll(x)
+#define pq_max priority_queue<ll>
+#define pq_min priority_queue<ll,vl,greater<ll>>
+#define fast() ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define pb push_back
+#define in insert
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+vector<vector<ll>>delta1={{1,0},{-1,0},{0,-1},{0,1}};
+vector<vector<ll>>delta2={{1,0},{-1,0},{0,-1},{0,1},{1,1},{-1,1},{1,-1},{-1,-1}};
+
 
 
 
